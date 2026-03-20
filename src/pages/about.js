d8b4e090b74a39f25016226291878dec2cc6dbfe
John Doe <johndoe@email.com>
2026-03-20 13:00:04 -0500

    added static image

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+const AboutPage = () => {
+    return (
+        <Layout>
+              <main>
+            <h1>About me</h1>
+            <p>Would love to visit a mountain one day</p>
+        </main>
+        </Layout>
+      
+    )
+}
+
+export const Head = () => <title>About</title>
+
+export default AboutPage
