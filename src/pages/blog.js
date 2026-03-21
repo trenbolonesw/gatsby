3e11b65ba998a3392e7154670af7b368e11ce849
John Doe <johndoe@email.com>
2026-03-21 11:58:47 -0500

    added jsx rendering for allmdx

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,10 +6,12 @@ const BlogPage = ({data}) => {
     return(
         <Layout pageTitle="My Blog Posts">
             <ul>
-         {data.allFile.nodes.map(node => (
-            <li key={node.name}>
-             {node.name}
-            </li>
+         {data.allMdx.nodes.map(node => (
+            <article key={node.id}>
+             <h2>{node.frontmatter.title}</h2>
+             <p>Posted: {node.frontmatter.date}</p>
+             <p>{node.excerpt}</p>
+            </article>
          ))}
          </ul>
         </Layout>
@@ -17,12 +19,19 @@ const BlogPage = ({data}) => {
 }
 
 export const query = graphql`
-query{
-allFile{
-nodes{
-name
-}
-}}`
+  query {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
+      }
+    }
+  }
+`
 
 export const Head = () => <Seo title="my blog posts"/>
 
